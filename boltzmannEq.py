b327f534bbf2fb985eaef7f9e9c39fe1c183f30c
Gabriela Alevi <129913205+gabrielaalevi@users.noreply.github.com>
2024-10-10 21:28:19 -0300

    Add files via upload

--- boltzmannEq.py
+++ boltzmannEq.py
@@ -3,9 +3,8 @@
 import numpy as np
 from scipy.special import kn
 import auxFunc
-from modelParameters import mDM
 
-def boltz(x, Y, comp_names, SM):
+def boltz(x, Y, comp_names, SM, mDM):
     dY = np.zeros(len(comp_names)) #list with all the rhs for all the components, ordered as in the comp_list
     H = auxFunc.hubblerate(x, mDM) #hubble rate at temperature T
     s = auxFunc.entropydensity(x, mDM) #entropy density at temperature T
@@ -37,10 +36,10 @@ def boltz(x, Y, comp_names, SM):
                         if (comp.decayreactions[j][0][l]) == comp_product.PDG:
                             dec_term *= Y[comp_product.ID]/comp_product.equilibriumyield(x, mDM)
                             product.append(comp_product.ID)
-                dY[i] += - (kn(1, comp.mass/T)/kn(2, comp.mass/T)) * (comp.decaywidth/s) * (Y[i] - comp.equilibriumyield(x, mDM) *dec_term)
+                dY[i] += (kn(1, comp.mass/T)/kn(2, comp.mass/T)) * (comp.decaywidth/s) * (Y[i] - comp.equilibriumyield(x, mDM) *dec_term)
                 for l in range(0, len(product)):
                 #adding the correspondent source term to the dY equation of each product
-                    dY[(product[l])] += (kn(1, comp.mass/T)/kn(2, comp.mass/T)) * (comp.decaywidth/s) * (Y[i] - comp.equilibriumyield(x, mDM)* dec_term)
+                    dY[(product[l])] += - (kn(1, comp.mass/T)/kn(2, comp.mass/T)) * (comp.decaywidth/s) * (Y[i] - comp.equilibriumyield(x, mDM)* dec_term)
         
     #collision term for component i
         for m in range(0, len(comp.collisions)):
@@ -54,28 +53,43 @@ def boltz(x, Y, comp_names, SM):
                 comp_product = comp_names[n]
                 if partner == comp_product.type:
                 #if the component we are analysing is the partner, we save it as the CLASS COMPONENT
-                    partner = comp_product
+                    partner_comp = comp_product
                 for p in range(0, len(comp.collisions[m][1])):
                 #loop parsing through all the products of collision m
                     if (comp.collisions[m][1][p]) == comp_product.type:
                     #if the component we are analysing is one of the products of the reaction, we add to the col_term and save its ID
                         col_term *= Y[comp_product.ID]/comp_product.equilibriumyield(x, mDM)
                         product.append(comp_product.ID)
-            if partner.type not in SM:
-                if partner.type == comp.type:
-                    dY[i] += (sigma_v/2) *((Y[i]**2) - (comp.equilibriumyield(x, mDM)**2) * col_term)
+            if sigma_v != 0:
+                #print('comp yeq:', comp.type, comp.equilibriumyield(x, mDM), 'partner yeq:', partner_comp.type , partner_comp.equilibriumyield(x, mDM))
+                #print('H', H, 'dsdx', dsdx)
+                if not product:
+                #condition if the product list is empty (i.e. the reaction only produces SM particles, which are in equilibrium)
+                    if partner_comp.type not in SM:
+                        if partner_comp.label == comp.label:
+                        #multiplying by 2 because that's what they do in MadDM, doing it just to compare the results
+                            dY[i] += (2 * sigma_v) *((Y[i]**2) - (comp.equilibriumyield(x, mDM)**2))
+                        else:
+                            dY[i] += sigma_v *((Y[i] * Y[partner_comp.ID]) - (comp.equilibriumyield(x, mDM) * partner_comp.equilibriumyield(x, mDM)))
+                            #print(sigma_v, Y[i], Y[partner_comp.ID], comp.equilibriumyield(x, mDM), partner_comp.equilibriumyield(x, mDM))
+                            #print(comp.type, partner_comp.type, sigma_v *((Y[i] * Y[partner_comp.ID]) - (comp.equilibriumyield(x, mDM) * partner_comp.equilibriumyield(x, mDM))), Y[comp.ID], Y[partner_comp.ID])
+                    if partner_comp.type in SM:
+                        dY[i] += sigma_v * ((Y[i]) - comp.equilibriumyield(x, mDM) * col_term)
                 else:
-                    dY[i] += - sigma_v *((Y[i] * Y[partner.ID]) - (comp.equilibriumyield(x, mDM) * partner.equilibriumyield(x, mDM)) * col_term)
-                    dY[partner.ID] += - sigma_v * ((Y[i] * Y[partner.ID]) - (comp.equilibriumyield(x, mDM) * partner.equilibriumyield(x, mDM)) * col_term)
-                for p in range(0, len(product)):
-                    dY[(product[p])] += sigma_v * ((Y[i]*Y[partner.ID]) - (comp.equilibriumyield(x, mDM) * partner.equilibriumyield(x, mDM)) * col_term)
-            if partner.type in SM:
-                dY[i] += sigma_v * ((Y[i]) - comp.equilibriumyield(x, mDM) * col_term)
-                for p in range(0, len(product)):
-                    dY[(product[p])] += sigma_v * (Y[i] - comp.equilibriumyield(x, mDM) * col_term)
+                #if the product list is not empty (i.e. we have BSM particles produced in this reaction)
+                    if partner_comp.type not in SM:
+                        if partner_comp.label == comp.label:
+                            dY[i] += (sigma_v/2) *((Y[i]**2) - (comp.equilibriumyield(x, mDM)**2) * col_term)
+                        else:
+                            dY[i] += sigma_v *((Y[i] * Y[partner_comp.ID]) - (comp.equilibriumyield(x, mDM) * partner_comp.equilibriumyield(x, mDM))*col_term)
+                        for p in range(0, len(product)):
+                            dY[(product[p])] += - sigma_v * (Y[i] * Y[partner_comp.ID] - (comp.equilibriumyield(x, mDM) * partner_comp.equilibriumyield(x, mDM)) * col_term)
+                    if partner_comp.type in SM:
+                        dY[i] += sigma_v * ((Y[i]) - comp.equilibriumyield(x, mDM) * col_term)
+                    for p in range(0, len(product)):
+                        dY[(product[p])] += - sigma_v * (Y[i] - comp.equilibriumyield(x, mDM) * col_term)
     for i in range(0, len(dY)):
-    #loop to multiply all the dY elements by the 1/3H * ds/dx factor. we chose to multiply it now in an
-    #attempt to simplify the numerical integration
+    #loop to multiply all the dY elements by the 1/3H * ds/dx factor
         dY[i] = (1/(3 * H)) * dsdx * dY[i]
     print('x', x, 'dY', dY)
     return dY
