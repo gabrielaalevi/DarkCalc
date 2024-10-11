4096f3d570a49c984d32ffb4b22429a348a89cf8
Gabriela Alevi <129913205+gabrielaalevi@users.noreply.github.com>
2024-10-11 15:37:20 -0300

    Add files via upload

--- auxFunc.py
+++ auxFunc.py
@@ -1,6 +1,7 @@
 #This module has auxiliary functions
 import math
 from scipy.special import kn, zeta
+import numpy as np
 m_planck = 2.4 * 10**(18) #reduced planck mass, in GeV
 
 #calculates the number of relativistic degrees of freedom for temperature T (in GeV), using solely the SM degrees of freedom
@@ -34,16 +35,16 @@ def equilibrium_yield(m, x, mDM, g):
     #and s represents entropy density.
     T = mDM/x
     g_star = degrees_of_freedom(x, mDM)#g_* is the number of relativistic degrees of freedom
-    #s = (2/45) * (math.pi**2) * (g_star) * T**3 #entropy density
-    #if (m/T)>10: #non-relativistic regime
-        #Y_eq = (abs(g) * (m * T/ (2 * math.pi))**(3/2) * math.exp(-m/T))/s
-    #elif (m/T)>(2/3): #semi-relativistic regime
-    Y_eq = (45 * abs(g) * (m/T)**2 * kn(2,(m/T)))/(4 * math.pi**4 * g_star)
-    #elif (m/T)<=(2/3): #relativistic regime
-        #if g > 0:
-           #Y_eq = ((3/4) * (zeta(3)/math.pi**2) * abs(g) * (T)**3)/s
-        #if g <= 0:
-            #Y_eq = ((zeta(3)/math.pi**2) * abs(g) * (T)**3)/s
+    s = (2/45) * (math.pi**2) * (g_star) * T**3 #entropy density
+    if (m/T)>5: #non-relativistic regime
+        Y_eq = (45/(4 * np.pi**4)) * (g/g_star) * (m/T)**(1.5) * np.sqrt(np.pi/2) * np.exp(-m/T) * (1 + (15/(8 * (m/T))) + 105/(128 * (m/T)**2) - (315/(1024 * (m/T)**3) )) #(abs(g) * (m * T/ (2 * math.pi))**(3/2) * math.exp(-m/T))/s
+    elif (m/T)>(2/3): #semi-relativistic regime
+        Y_eq = (45 * abs(g) * (m/T)**2 * kn(2,(m/T)))/(4 * math.pi**4 * g_star)
+    elif (m/T)<=(2/3): #relativistic regime
+        if g > 0: #fermions
+           Y_eq = ((3/4) * (zeta(3)/math.pi**2) * abs(g) * (T)**3)/s
+        if g <= 0: #bosons
+            Y_eq = ((zeta(3)/math.pi**2) * abs(g) * (T)**3)/s
     return Y_eq
 
 #calculates the variation of entropy with x, where x = m_DM/T
